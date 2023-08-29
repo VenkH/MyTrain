2709942cd111d55b535c910d208e863877188698
kyr <ke.per@qq.com>
2023-08-29 22:27:32 +0800

    init

---
+++ src/main/java/com/kyr/mytrain/MyTrainApplication.java
@@ -0,0 +1,13 @@
+package com.kyr.mytrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyr/mytrain/MyTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyr.mytrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
