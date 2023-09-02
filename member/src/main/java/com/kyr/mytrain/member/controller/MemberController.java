96ec346f7ae095419f551408f682ebaf2a577bc7
kyr <ke.per@qq.com>
2023-09-02 20:04:45 +0800

    fix(member): 修改发送验证码和登录两个接口的参数传递方式

--- member/src/main/java/com/kyr/mytrain/member/controller/MemberController.java
+++ member/src/main/java/com/kyr/mytrain/member/controller/MemberController.java
@@ -6,8 +6,9 @@ import com.kyr.mytrain.member.dto.MemberRegisterDto;
 import com.kyr.mytrain.member.dto.SendCodeDto;
 import com.kyr.mytrain.member.service.MemberService;
 import jakarta.annotation.Resource;
-import org.springframework.validation.annotation.Validated;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +20,17 @@ public class MemberController {
     private MemberService memberService;
 
     @PostMapping("/register")
-    public CommonResp<Long> register(@Validated MemberRegisterDto mobile) {
+    public CommonResp<Long> register(@Valid MemberRegisterDto mobile) {
         return memberService.register(mobile);
     }
 
     @PostMapping("/sendCode")
-    public CommonResp sendCode(@Validated SendCodeDto mobile) {return memberService.sendCode(mobile);}
+    public CommonResp sendCode(@Valid @RequestBody SendCodeDto mobile) {
+        return memberService.sendCode(mobile);
+    }
 
     @PostMapping("/login")
-    public CommonResp login(@Validated LoginDto loginDto) {return memberService.login(loginDto);}
+    public CommonResp login(@Valid @RequestBody LoginDto loginDto) {
+        return memberService.login(loginDto);
+    }
 }
