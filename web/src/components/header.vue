e7cd2e683ffbc9ce65ec541c491e115c71684504
kyr <ke.per@qq.com>
2023-09-04 00:07:57 +0800

    fix(web): 修改顶部和侧边栏的菜单

--- web/src/components/header.vue
+++ web/src/components/header.vue
@@ -13,9 +13,16 @@
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 11</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined/>&nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <user-outlined/>&nbsp;乘车人管理
+        </router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
