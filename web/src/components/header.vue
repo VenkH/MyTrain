245913faa23a3c32ef8aa3bd41a9829123735900
kyr <ke.per@qq.com>
2023-09-03 14:18:03 +0800

    feat(web): vue组件开发，增加header组件

---
+++ web/src/components/header.vue
@@ -0,0 +1,46 @@
+<template>
+  <a-layout-header class="header" id="components-layout-demo-top-side-2">
+    <div class="logo" />
+    <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 11</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { ref } from 'vue';
+const selectedKeys1 = ref(['2']);
+
+export default defineComponent({
+  name: "header-view",
+  setup() {
+    return {
+      selectedKeys1,
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+</style>
--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -1,18 +1,6 @@
 <template>
-  <a-layout>
-    <a-layout-header class="header" id="components-layout-demo-top-side-2">
-      <div class="logo" />
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+  <a-layout >
+   <header-view></header-view>
     <a-layout>
       <a-layout-sider width="200" style="background: #fff">
         <a-menu
@@ -75,24 +63,12 @@
   </a-layout>
 </template>
 <script>
-// import { ref } from 'vue';
-// const selectedKeys1 = ref(['2']);
-// const selectedKeys2 = ref(['1']);
-// const openKeys = ref(['sub1']);
+import HeaderView from "@/components/header";
+export default {
+  components: {HeaderView}
+}
 </script>
 <style scoped>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
-
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
 
 .site-layout-background {
   background: #fff;
