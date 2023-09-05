797d9e839074ce684d8c6b6509e785e5a6883b62
kyr <ke.per@qq.com>
2023-09-05 18:46:38 +0800

    fix(member): 保存乘车人接口接收前端参数时，将日期格式化

--- member/src/main/java/com/kyr/mytrain/member/dto/PassengerSaveDto.java
+++ member/src/main/java/com/kyr/mytrain/member/dto/PassengerSaveDto.java
@@ -1,5 +1,6 @@
 package com.kyr.mytrain.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.Date;
@@ -18,8 +19,10 @@ public class PassengerSaveDto {
     @NotBlank(message = "【类型】不能为空")
     private String type;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
