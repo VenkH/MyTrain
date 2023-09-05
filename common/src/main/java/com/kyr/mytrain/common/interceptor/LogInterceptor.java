54eb953fa250af4fd521e21e47a77a3f345f22f8
kyr <ke.per@qq.com>
2023-09-05 20:24:55 +0800

    fix(common): 修改日志开始位置

--- common/src/main/java/com/kyr/mytrain/common/interceptor/LogInterceptor.java
+++ common/src/main/java/com/kyr/mytrain/common/interceptor/LogInterceptor.java
@@ -3,6 +3,7 @@ package com.kyr.mytrain.common.interceptor;
 import cn.hutool.core.util.RandomUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -11,12 +12,14 @@ import org.springframework.web.servlet.HandlerInterceptor;
  * 日志拦截器
  */
 @Component
+@Slf4j
 public class LogInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 增加日志流水号
         MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        log.info("------------- 开始 -------------");
         return true;
     }
 
