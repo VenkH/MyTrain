234832c692911189d64b0912ccd9430e3011a3bc
kyr <ke.per@qq.com>
2023-09-20 20:04:38 +0800

    feat(admin): 为每日车次页面增加日期和车次查询条件

--- admin/src/views/main/business/daily-train.vue
+++ admin/src/views/main/business/daily-train.vue
@@ -1,7 +1,9 @@
 <template>
   <p>
     <a-space>
-      <a-button type="primary" @click="handleQuery()">刷新</a-button>
+      <a-date-picker v-model:value="params.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" />
+      <train-select-view v-model="params.code" width="200px"></train-select-view>
+      <a-button type="primary" @click="handleQuery()">查询</a-button>
       <a-button type="primary" @click="onAdd">新增</a-button>
     </a-space>
   </p>
@@ -101,6 +103,10 @@ export default defineComponent({
       total: 0,
       current: 1,
       pageSize: 10,
+    });
+    let params = ref({
+      code: null,
+      date: null
     });
     let loading = ref(false);
     const columns = [
@@ -207,7 +213,9 @@ export default defineComponent({
       axios.get("/business-service/admin/daily-train/query-list", {
         params: {
           page: param.page,
-          size: param.size
+          size: param.size,
+          code: params.value.code,
+          date: params.value.date
         }
       }).then((response) => {
         loading.value = false;
@@ -260,7 +268,8 @@ export default defineComponent({
       handleOk,
       onEdit,
       onDelete,
-      onChangeCode
+      onChangeCode,
+      params
     };
   },
 });
--- business/src/main/java/com/kyr/mytrain/business/req/DailyTrainQueryReq.java
+++ business/src/main/java/com/kyr/mytrain/business/req/DailyTrainQueryReq.java
@@ -1,8 +1,32 @@
 package com.kyr.mytrain.business.req;
 
 import com.kyr.mytrain.common.dto.PageDto;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 public class DailyTrainQueryReq extends PageDto {
+
+    private String code;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
     @Override
     public String toString() {
