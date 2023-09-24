91b92a0837a7715fdb14063e8d0297c6526dd599
kyr <ke.per@qq.com>
2023-09-24 14:50:47 +0800

    feat(web): 美化车次信息的显示

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>{{ dailyTrainTicket }}</div>
+  <div class="order-train">
+    <span class="order-train-main">{{dailyTrainTicket.date}}</span>&nbsp;
+    <span class="order-train-main">{{dailyTrainTicket.trainCode}}</span>次&nbsp;
+    <span class="order-train-main">{{dailyTrainTicket.start}}</span>站
+    <span class="order-train-main">({{dailyTrainTicket.startTime}})</span>&nbsp;
+    <span class="order-train-main">——</span>&nbsp;
+    <span class="order-train-main">{{dailyTrainTicket.end}}</span>站
+    <span class="order-train-main">({{dailyTrainTicket.endTime}})</span>&nbsp;
+  </div>
 </template>
 
 <script>
@@ -18,3 +26,10 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+.order-train .order-train-main {
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
