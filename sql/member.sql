f43763eab35f1b1a2ff2cf56ed8077044148e1ce
kyr <ke.per@qq.com>
2023-10-09 12:34:46 +0800

    feat(sql)：member表增加给seata使用的undo_log表

--- sql/member.sql
+++ sql/member.sql
@@ -18,3 +18,40 @@ create table `passenger` (
   primary key (`id`),
   index `member_id_index` (`member_id`)
 ) engine=innodb default charset=utf8mb4 comment='乘车人';
+
+drop table if exists `ticket`;
+create table `ticket` (
+                          `id` bigint not null comment 'id',
+                          `member_id` bigint not null comment '会员id',
+                          `passenger_id` bigint not null comment '乘客id',
+                          `passenger_name` varchar(20) comment '乘客姓名',
+                          `train_date` date not null comment '日期',
+                          `train_code` varchar(20) not null comment '车次编号',
+                          `carriage_index` int not null comment '箱序',
+                          `seat_row` char(2) not null comment '排号|01, 02',
+                          `seat_col` char(1) not null comment '列号|枚举[SeatColEnum]',
+                          `start_station` varchar(20) not null comment '出发站',
+                          `start_time` time not null comment '出发时间',
+                          `end_station` varchar(20) not null comment '到达站',
+                          `end_time` time not null comment '到站时间',
+                          `seat_type` char(1) not null comment '座位类型|枚举[SeatTypeEnum]',
+                          `create_time` datetime(3) comment '新增时间',
+                          `update_time` datetime(3) comment '修改时间',
+                          primary key (`id`),
+                          index `member_id_index` (`member_id`)
+) engine=innodb default charset=utf8mb4 comment='车票';
+
+
+CREATE TABLE `undo_log` (
+                            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                            `branch_id` bigint(20) NOT NULL,
+                            `xid` varchar(100) NOT NULL,
+                            `context` varchar(128) NOT NULL,
+                            `rollback_info` longblob NOT NULL,
+                            `log_status` int(11) NOT NULL,
+                            `log_created` datetime NOT NULL,
+                            `log_modified` datetime NOT NULL,
+                            `ext` varchar(100) DEFAULT NULL,
+                            PRIMARY KEY (`id`),
+                            UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
